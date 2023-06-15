2b15d8a7c3215730e1845575401cffbd021152da
BBtanapon <95513445+BBtanapon@users.noreply.github.com>
2023-06-15 13:52:31 +0700

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    var dataTable = $('#filtertable').DataTable({
-        "pageLength":5,
-        'aoColumnDefs':[{
-            'bSortable':false,
-            'aTargets':['nosort'],
-        }],
-    });
-} );
